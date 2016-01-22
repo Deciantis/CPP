194029b02c35b69039ca37c231c1c3044af9d1a8
D. Brutus <deciantis34@gmail.com>
2016-01-21 22:43:03 -0500

    Simple Hello world from Xcode IDE

---
+++ CPP/CPP/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  CPP Refresher
+/*
+ This program outputs Hello World,
+ in the console.
+ */
+//  Created by Dee Bruce on 1/21/16.
+//  Copyright © 2016 com. All rights reserved.
+//
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+
+    std::cout << "Hello, World!\n";
+    
+    cout << "Yellow World!\n";
+    return 0;
+}
